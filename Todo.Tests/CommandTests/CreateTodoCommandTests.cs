e4d4505e63bde5c62b5803deda45b01504059f8a
iwtapcgmr <69200078+iwtapcgmr@users.noreply.github.com>
2022-03-30 22:09:40 -0300

    Iniciando com tests

---
+++ Todo.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Commands.TodoCommands;
+
+namespace Todo.Tests.CommandTests
+{
+    [TestClass]
+    public class CreateTodoCommandTests
+    {
+        [TestMethod]
+        public void Dado_um_comando_invalido()
+        {
+            // Arrange
+            var command = new CreateTodoCommand("", "", DateTime.UtcNow);
+
+            // Act
+            command.Validate();
+
+            // Assert
+            Assert.AreEqual(command.Valid, false);
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido()
+        {
+            // Arrange
+            var command = new CreateTodoCommand("This is a valid Title", "UserValid", DateTime.UtcNow);
+
+            // Act
+            command.Validate();
+
+            // Assert
+            Assert.AreEqual(command.Valid, true);
+        }
+    }
+}

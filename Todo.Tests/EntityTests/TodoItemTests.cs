880741111b04813aa2b4d83893b448d9972bc7ae
iwtapcgmr <69200078+iwtapcgmr@users.noreply.github.com>
2022-03-31 06:55:43 -0300

    Testes de entidade.

---
+++ Todo.Tests/EntityTests/TodoItemTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Entities;
+
+namespace Todo.Tests.EntityTests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        private readonly TodoItem _todoItem = new TodoItem("Title", DateTime.UtcNow, "User");
+
+        [TestMethod]
+        public void Dado_um_novo_todo_done_deve_ser_false()
+        {
+            // Assert
+            Assert.AreEqual(_todoItem.Done, false);
+        }
+    }
+}

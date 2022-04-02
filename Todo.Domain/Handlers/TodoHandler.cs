1c3b853e5761bf8ca1648dcbfc3773251f364c10
iwtapcgmr <69200078+iwtapcgmr@users.noreply.github.com>
2022-04-01 22:26:27 -0300

    Inserindo no TodoHandler os métodos de marcar como concluído e marcar como não concluido

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -9,7 +9,9 @@ namespace Todo.Domain.Handlers
 {
     public class TodoHandler :
                 IHandler<CreateTodoCommand>,
-                IHandler<UpdateTodoCommand>
+                IHandler<UpdateTodoCommand>,
+                IHandler<MarkTodoAsDoneCommand>,
+                IHandler<MarkTodoAsUndoneCommand>
     {
         private readonly ITodoRepository _todoRepostiory;
 
@@ -48,6 +50,52 @@ namespace Todo.Domain.Handlers
 
             // Altera o titulo
             todoItem.UpdateTitle(command.Title);
+
+            // Alterar no banco
+            await _todoRepostiory.Upadate(todoItem);
+
+            // Devolver
+            return new GenericCommandResult(true, "Tarefa alterada com sucesso!", todoItem);
+        }
+
+        public async Task<ICommandResult> Handle(MarkTodoAsDoneCommand command)
+        {
+            // Recupera a todo do banco (Reidratação)
+            // Reidratação: buscar os dados mais frescos do banco, pois os dados da tela podem estar desatualizados
+            var todoItem = await _todoRepostiory.GetByIdAsync(command.Id, command.User);
+
+            // Verifica se o status da tarefa foi alterado. Caso não, retorna erro e não executa o update
+            if (todoItem.Done)
+            {
+                command.AddNotification("Done", "Is true");
+                return new GenericCommandResult(false, "O status da tarefa não foi alterado.", command.Notifications);
+            }
+
+            // Altera o status
+            todoItem.MarkAsDone();
+
+            // Alterar no banco
+            await _todoRepostiory.Upadate(todoItem);
+
+            // Devolver
+            return new GenericCommandResult(true, "Tarefa alterada com sucesso!", todoItem);
+        }
+
+        public async Task<ICommandResult> Handle(MarkTodoAsUndoneCommand command)
+        {
+            // Recupera a todo do banco (Reidratação)
+            // Reidratação: buscar os dados mais frescos do banco, pois os dados da tela podem estar desatualizados
+            var todoItem = await _todoRepostiory.GetByIdAsync(command.Id, command.User);
+
+            // Verifica se o status da tarefa foi alterado. Caso não, retorna erro e não executa o update
+            if (!todoItem.Done)
+            {
+                command.AddNotification("Done", "Is false");
+                return new GenericCommandResult(false, "O status da tarefa não foi alterado.", command.Notifications);
+            }
+
+            // Altera o status
+            todoItem.MarkAsUndone();
 
             // Alterar no banco
             await _todoRepostiory.Upadate(todoItem);

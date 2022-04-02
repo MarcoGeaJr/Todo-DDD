4c1cd4a5339ae82767eeab20e5f020262b74371e
iwtapcgmr <69200078+iwtapcgmr@users.noreply.github.com>
2022-04-01 22:35:20 -0300

    Iniciando os testes dos metodos de alteracao do todo.

---
+++ Todo.Tests/CommandTests/MarkAsDoneCommandTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Todo.Tests.CommandTests
+{
+    [TestClass]
+    public class MarkAsDoneCommandTests
+    {
+        [TestMethod]
+        public void UnitTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Todo.Tests/CommandTests/MarkAsUndoneCommandTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Todo.Tests.CommandTests
+{
+    [TestClass]
+    public class MarkAsUndoneCommandTests
+    {
+        [TestMethod]
+        public void UnitTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

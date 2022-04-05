96eb266371a9e61f538e1e4738538aad21368fad
iwtapcgmr <69200078+iwtapcgmr@users.noreply.github.com>
2022-04-04 22:12:09 -0300

    Introduzindo ef core a mapeamentos.

---
+++ Todo.Infra/Contexts/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+using Todo.Infra.Mappings;
+
+namespace Todo.Infra.Contexts
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TodoItemMapping());
+        }
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
---
+++ Todo.Infra/Mappings/TodoItemMapping.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Todo.Domain.Entities;
+
+namespace Todo.Infra.Mappings
+{
+    public class TodoItemMapping : IEntityTypeConfiguration<TodoItem>
+    {
+        public void Configure(EntityTypeBuilder<TodoItem> builder)
+        {
+            builder.ToTable("Todo");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.User)
+                    .HasColumnType("varchar(120)")
+                    .HasMaxLength(120);
+            builder.Property(x => x.Title)
+                   .HasMaxLength(160)
+                   .HasColumnType("varchar(160)");
+            builder.Property(x => x.Done)
+                   .HasColumnType("bit");
+
+            builder.Property(x => x.Date);
+
+            builder.HasIndex(x => x.User);
+        }
+    }
+}

0e6dbabdad2aff74e7a274d59eda975a7f4f0003
iwtapcgmr <69200078+iwtapcgmr@users.noreply.github.com>
2022-04-07 06:54:20 -0300

    Iniciando a controller.

---
+++ Todo.Api/Controllers/TodoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Shared.Commands;
+
+namespace Todo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpPost("")]
+        public ActionResult<GenericCommandResult> Create()
+        {
+            return null;
+        }
+    }
+}
--- Todo.Tests/Repositories/FakeTodoRepository.cs
+++ Todo.Tests/Repositories/FakeTodoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Todo.Domain.Entities;
 using Todo.Domain.Repositories;
@@ -12,9 +13,29 @@ namespace Todo.Tests.Repositories
             return Task.CompletedTask;
         }
 
+        public Task<IEnumerable<TodoItem>> GetAll(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TodoItem>> GetAllDone(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TodoItem>> GetAllUnDone(string user)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<TodoItem> GetByIdAsync(Guid id, string user)
         {
             return Task.FromResult<TodoItem>(new TodoItem("Title", DateTime.UtcNow, user));
+        }
+
+        public Task<IEnumerable<TodoItem>> GetByPeriod(string user, DateTime time, bool done)
+        {
+            throw new NotImplementedException();
         }
 
         public Task Upadate(TodoItem todo)
